fffb57090acd2f0ee2e55fd4be5d8fc951bab2aa
JojiKoike <joji.koike@gmail.com>
2018-09-10 03:20:02 +0900

    Using Traits Sample

---
+++ src/main/scala/Animal.scala
@@ -0,0 +1,9 @@
+class Animal
+trait HasLegs
+class Frog extends Animal with Philosophical with HasLegs {
+  override def toString: String = "green"
+
+  override def philosophize(): Unit = {
+    println("It ain't easy being " + toString + "!")
+  }
+}
