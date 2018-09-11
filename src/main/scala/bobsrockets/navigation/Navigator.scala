eab14975b1ccbfff656bf8e9846e1329be8acd5d
JojiKoike <joji.koike@gmail.com>
2018-09-11 21:56:33 +0900

    Access Modifier Sample

--- src/main/scala/bobsrockets/navigation/Navigator.scala
+++ src/main/scala/bobsrockets/navigation/Navigator.scala
@@ -1,5 +1,11 @@
 package bobsrockets.navigation
 
-class Navigator {
+private[bobsrockets] class Navigator {
   val map: StarMap = new StarMap
+  protected[navigation] def userStarChart(): Unit = {}
+  class LegOfJourney {
+    private[Navigator] val distance = 100
+  }
+  private[this] var speed = 200
+
 }
---
+++ src/main/scala/bobsrockets/navigation/launch/Vehicle.scala
@@ -0,0 +1,6 @@
+package bobsrockets.navigation.launch
+import bobsrockets.navigation._
+
+object Vehicle {
+  private[launch] val guide = new Navigator
+}
