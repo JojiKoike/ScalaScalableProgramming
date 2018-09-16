01f3e1f7168c701f97a8935d228328cd48f69ef6
JojiKoike <joji.koike@gmail.com>
2018-09-16 21:38:45 +0900

    Move To list_methods.scala

--- src/script/append.scala
+++
@@ -1,7 +0,0 @@
-def append[T] (xs: List[T], ys: List[T]): List[T] =
-  xs match {
-    case List() => ys
-    case x :: xs1 => x :: append(xs1, ys)
-  }
-
-append(List(), List(1,2,3)).foreach(print)
