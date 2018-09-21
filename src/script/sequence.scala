f7f8e93176f3b163255958c880efe7e49a217a84
JojiKoike <joji.koike@gmail.com>
2018-09-22 01:00:47 +0900

    List Buffer Sample

--- src/script/sequence.scala
+++ src/script/sequence.scala
@@ -16,4 +16,13 @@ fiveInts.foreach(print)
 println
 
 // List Buffer
-
+val buf = new scala.collection.mutable.ListBuffer[Int]
+// Add Tail
+buf += 1
+buf += 2
+buf.toList.foreach(print)
+println
+// Add Head
+3 +=: buf
+buf.toList.foreach(print)
+println
