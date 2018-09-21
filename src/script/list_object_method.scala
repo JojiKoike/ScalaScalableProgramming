6e9592f7c6717209935d340860cedace121a44f6
JojiKoike <joji.koike@gmail.com>
2018-09-21 23:35:45 +0900

    fill, tabulate, concat samples

--- src/script/list_object_method.scala
+++ src/script/list_object_method.scala
@@ -5,3 +5,16 @@ println(List.apply(1,2,3))
 println(List.range(1,5))
 println(List.range(1, 9, 2))
 println(List.range(9, 1, -3))
+
+// fill : all elements are filled with same value
+println(List.fill(5)('a'))
+println(List.fill(3)("Hello"))
+println(List.fill(2, 2)("Test"))
+
+// tabulate : all elements are filled with the value calculated by specified function
+println(List.tabulate(5)(n => n * n))
+println(List.tabulate(5,5)(_ * _))
+
+// concat
+println(List.concat(List('a', 'b') , List('c')))
+println(List.concat(List(), List('b'), List('c')))
