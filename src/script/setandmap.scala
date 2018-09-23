0274de63a870baa592a49fe0157ef4cfd10fd9f5
JojiKoike <joji.koike@gmail.com>
2018-09-24 07:54:28 +0900

    Set Samples

--- src/script/setandmap.scala
+++ src/script/setandmap.scala
@@ -1,3 +1,16 @@
+// Immutable Set
+val immutableSet = Set(1,2,3)
+println(immutableSet)
+
 // Mutable Set
-val mutableSet = scala.collection.mutable.Set(1,2,3)
+val mutableSet = scala.collection.mutable.Set(4,5,6)
+mutableSet += 7
 println(mutableSet)
+
+// Set Use Sample
+val text = "See Spot run. Run, Spot. Run!"
+val wordsArray = text.split("[ !,.]+")
+val words = scala.collection.mutable.Set.empty[String]
+for (word <- wordsArray)
+  words += word.toLowerCase
+println(words.mkString(","))
