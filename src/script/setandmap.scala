b792e4e3f1f50f97ca8288f2eb87a0b725cbd89c
JojiKoike <joji.koike@gmail.com>
2018-09-24 12:11:35 +0900

    Map, Immutable, Mutable List Samples

--- src/script/setandmap.scala
+++ src/script/setandmap.scala
@@ -1,11 +1,29 @@
 // Immutable Set
 val immutableSet = Set(1,2,3)
 println(immutableSet)
+val nums = Set(1,2,3)
+println(nums)
+println(nums + 5)
+println(nums)
+println(nums - 3)
+println(nums)
+println(nums.size)
+println(nums.contains(3))
 
 // Mutable Set
 val mutableSet = scala.collection.mutable.Set(4,5,6)
 mutableSet += 7
 println(mutableSet)
+val texts = scala.collection.mutable.Set.empty[String]
+println(texts)
+println(texts += "the")
+println(texts)
+println(texts -= "the")
+println(texts)
+println(texts ++= List("a", "b", "c", "d"))
+println(texts)
+println(texts.clear)
+println(texts)
 
 // Set Use Sample
 val text = "See Spot run. Run, Spot. Run!"
