829018a3cbb68b5b1c0117241eace5cc5df4ed97
JojiKoike <joji.koike@gmail.com>
2018-09-27 05:31:35 +0900

    Mutable Object Sample

--- src/main/scala/BankCustomer.scala
+++ src/main/scala/BankCustomer.scala
@@ -2,4 +2,6 @@ object BankCustomer extends App {
   val account = new BankAccount
   account deposit(100)
   println(account.balance)
+  println(account withdraw(80))
+  println(account withdraw(80))
 }
