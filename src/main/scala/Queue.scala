309328ab8e2c4854d10575c73849a1ab991742ec
JojiKoike <joji.koike@gmail.com>
2018-09-28 03:56:25 +0900

    Type Params Sample using Trait, private class

--- src/main/scala/Queue.scala
+++ src/main/scala/Queue.scala
@@ -1,16 +1,27 @@
-class Queue[T] (private val leading: List[T], private val trailing: List[T]) {
+trait Queue[T] {
+  def head: T
+  def tail: Queue[T]
+  def enqueue(x: T): Queue[T]
+}
 
-  private def mirror =
-    if (leading.isEmpty)
-      new Queue(trailing.reverse, Nil)
-    else
-      this
+object Queue {
+  def apply[T](xs :T*) = new QueueImpl[T](xs.toList, Nil)
 
-  def head: T = mirror.leading.head
-  def tail: Queue[T] = {
-    val q = mirror
-    new Queue(q.trailing.tail, q.trailing)
+  private class QueueImpl[T](private val leading: List[T],
+                               private val trailing: List[T]) extends Queue[T] {
+
+    private def mirror: QueueImpl[T] =
+      if (leading.isEmpty)
+        new QueueImpl(trailing.reverse, Nil)
+      else
+        this
+
+    def head: T = mirror.leading.head
+    def tail: Queue[T] = {
+      val q = mirror
+      new QueueImpl(q.trailing.tail, q.trailing)
+    }
+    def enqueue(x: T) =
+      new QueueImpl(leading, x :: trailing)
   }
-  def enqueue(x: T) =
-    new Queue(leading, x :: trailing)
 }
