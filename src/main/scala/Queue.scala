662f5d546d4e5e67de93d4bfc80918fb573dfbe5
JojiKoike <joji.koike@gmail.com>
2018-09-29 23:31:09 +0900

    Add apply function output Type

--- src/main/scala/Queue.scala
+++ src/main/scala/Queue.scala
@@ -4,8 +4,9 @@ trait Queue[+T] {
   def enqueue[U >: T](x:  U): Queue[U]
 }
 
+
 object Queue {
-  def apply[T](xs :T*) = new QueueImpl[T](xs.toList, Nil)
+  def apply[T](xs :T*): Queue[T] = new QueueImpl[T](xs.toList, Nil)
 
   private class QueueImpl[T](private val leading: List[T],
                                private val trailing: List[T]) extends Queue[T] {
