66cd55dd1c00993ca59e02ef9e6a04da83579615
JojiKoike <joji.koike@gmail.com>
2018-10-09 06:42:15 +0900

    Sequence Sample : Patch, Updated, Sort, Comparison

--- src/main/scala/chapter24/C245.scala
+++ src/main/scala/chapter24/C245.scala
@@ -16,4 +16,21 @@ object C245 extends App {
   println((10 +: seqSample).mkString(","))
   println((seqSample :+ 100).mkString(","))
 
+  // Patch
+  println((seqSample patch(1, Seq(4, 5, 6), 2)).mkString(","))
+
+  // Updated
+  println((seqSample updated (2, 4)).mkString(","))
+
+  // Sort
+  println((seqSample sorted).mkString(","))
+  println((seqSample reverse).mkString(","))
+
+  // Comparison
+  println(seqSample startsWith Seq(1, 2))
+  println(seqSample startsWith Seq(3, 4))
+  println(seqSample contains 3)
+  println(seqSample containsSlice Seq(3, 4, 5))
+
+
 }
